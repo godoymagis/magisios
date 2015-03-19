5a1f6bb85a2dea16e883df36b3ee082e403b92cd
Jose Godoy <JoseGodoy@wireless1x-155-41-118-18.bu.edu>
2015-03-18 20:36:59 -0400

    added files

---
+++ Magis/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Magis
+//
+//  Created by Jose Godoy on 3/18/15.
+//  Copyright (c) 2015 Global App Initiative. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Magis/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Magis
+//
+//  Created by Jose Godoy on 3/18/15.
+//  Copyright (c) 2015 Global App Initiative. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
